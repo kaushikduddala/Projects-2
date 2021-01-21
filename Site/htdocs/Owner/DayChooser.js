ded6bb889fac3cfd26870787f0dc660c1f582d9f
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 14:30:10 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/DayChooser.js
+++ Site/htdocs/Owner/DayChooser.js
@@ -9,7 +9,7 @@ if (date2 == 1 || date2 == 2 || date2 == 3){
 } else{ 
     alert("Error: Day is not part of the required days.")
     function redirectBack(){
-        
+        window.location.href = 
     }
     window.setTimeout(redirectBack, seconds);
 }
