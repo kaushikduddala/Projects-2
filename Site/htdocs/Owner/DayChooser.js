77f604dfd6e228cbf9c7fe799071aefed593bd06
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 14:38:13 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/DayChooser.js
+++ Site/htdocs/Owner/DayChooser.js
@@ -1,7 +1,7 @@
 <script> //starts the script
     date1 = new Date() //gets the current date
-date2 = date.getDay() // gets the day (0-6)
-if (date2 == 1 || date2 == 2 || date2 == 3){ //detects if its A day (monday )
+    date2 = date.getDay() // gets the day (0-6)
+    if (date2 == 1 || date2 == 2 || date2 == 3){ //detects if its A day (monday )
     window.location.href = "www.kaushiksites.epizy.com/Owner/Class%20Links%20(A%20day).html"
 } else if (date2 == 4){
     window.location.href = "https://www.kaushiksites.epizy.com/Owner/Class%20Links%20(B%20day%201).html"
