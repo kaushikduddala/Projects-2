2956a19de42aa94fc0d291f804b3f8fecf4b765c
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 14:37:25 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/DayChooser.js
+++ Site/htdocs/Owner/DayChooser.js
@@ -1,4 +1,4 @@
-<script type="text/javascript"></script>
+<script type="text/javascript">
 date1 = new Date() //gets the current date
 date2 = date.getDay() // gets the day (0-6)
 if (date2 == 1 || date2 == 2 || date2 == 3){ //detects if its A day (monday )
@@ -14,4 +14,5 @@ if (date2 == 1 || date2 == 2 || date2 == 3){ //detects if its A day (monday )
     }
     window.setTimeout(redirectBack(), 4);
 }
+
     
