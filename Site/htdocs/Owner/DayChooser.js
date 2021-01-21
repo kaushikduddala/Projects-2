ed4332b4219441c8bfc9a03283c7a515b36db350
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 14:25:04 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/DayChooser.js
+++ Site/htdocs/Owner/DayChooser.js
@@ -2,7 +2,7 @@ date1 = new Date()
 date2 = date.getDay()
 if (date2 > 0 && date2 < 4){
     window.location.href = "www.kaushiksites.epizy.com/Owner/Class%20Links%20(A%20day).html"
-} else (date2 == 4){
+} else if (date2 == 4){
     window.location.href = "https://www.kaushiksites.epizy.com/Owner/Class%20Links%20(B%20day%201).html"
 }
     
