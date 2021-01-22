35c0bbacb6a95d8e673e3254e0dc94ab2bae8ac5
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 21:54:41 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Class Links (B DAY 1).js
+++ Site/htdocs/Owner/Class Links (B DAY 1).js
@@ -1,4 +1,4 @@
-// this was mean for a page on my website to auto-open zoom links (this was in quarantine)
+// this was meant for a page on my website to auto-open zoom links (this was in quarantine)
 function reloadPage() { //setting reload function (for reloading for better results)
     window.location.href = "https://www.kaushiksites.epizy.com/Owner/Class%20Links%20(A%20day).html" } //opens the page again
 setTimeout(() => {reloadPage(); }, 1800000); //sets timeout for some time
