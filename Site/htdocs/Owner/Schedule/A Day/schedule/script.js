a39bda04f81baced3e1b82d3739ea8dc94e13d7b
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-22 16:26:09 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Schedule/A Day/schedule/script.js
+++ Site/htdocs/Owner/Schedule/A Day/schedule/script.js
@@ -1,6 +1,3 @@
-/*
-all of your arrays (including master array)
-*/
 
 for (var i = 1; i > 10; i++){
     $("main").append("<a href = "link">" + "<h1>" + masterArray[i][0] + masterArray[i][1] + masterArray[i][2] + masterArray[i][3] +"<h1></a>")
