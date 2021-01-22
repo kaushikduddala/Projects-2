95f6394829ec2f4a0036023e0fcb1a343719dee7
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 20:04:07 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Class Links (A DAY).js
+++ Site/htdocs/Owner/Class Links (A DAY).js
@@ -1,4 +1,4 @@
-function reloadPage() {
+function reloadPage() { //
     window.location.href = "https://www.kaushiksites.epizy.com/Owner/Class%20Links%20(A%20day).html" } 
 setTimeout(() => {reloadPage(); }, 1800000);
 var now = new Date();
