541ada1221f26df0ca084e3e17829817bf94151b
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 21:43:18 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Class Links (B DAY 1).js
+++ Site/htdocs/Owner/Class Links (B DAY 1).js
@@ -7,7 +7,7 @@ function autoOpen(Hour, Minute, Number, link){ //Sets the function with the vari
     var Number = new Date(now.getFullYear(), now.getMonth(), now.getDate(), Hour, Minute, 0, 0) - now; // The requested time - the current time = the time till it needs to open
     if (Number <= 0){  //if its below 0
         Number = 86400000 // sets it to a day so that it wont open and will take enough time to reloa and keep delaying it
-    }
+    } //closing
     function OpenPageJoin1(){
        window.location.href = link;
     }
