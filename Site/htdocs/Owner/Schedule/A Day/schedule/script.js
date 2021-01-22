d3f30e44de40d182c695a90dbd264b6143cfe3cd
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-22 17:13:08 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Schedule/A Day/schedule/script.js
+++ Site/htdocs/Owner/Schedule/A Day/schedule/script.js
@@ -1,6 +1,6 @@
 format = ['teacher', 'startTime', 'endTIme', 'class']
 links = []
-class1 = ['Mr. Travis.', '8:25 ', '9:08 ', ' 1st Period.']
+class1 = ['Mr. Travis.', '8:25 ', '9:08. ', ' 1st Period.']
 class2 = ['Mr. Daniel. ', '9:12', '9:55. ', ' 2nd Period.' ]
 class3 = ['Ms. Watts. ', '9:59 ', '11:29.', ' 3rd Period']
 class4 = []
