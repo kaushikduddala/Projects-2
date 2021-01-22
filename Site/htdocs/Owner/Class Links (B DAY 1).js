02db508dc797114e07c84a961e38f391b44cfab5
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 21:51:38 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Class Links (B DAY 1).js
+++ Site/htdocs/Owner/Class Links (B DAY 1).js
@@ -2,10 +2,10 @@
 function reloadPage() { //setting reload function (for reloading for better results)
     window.location.href = "https://www.kaushiksites.epizy.com/Owner/Class%20Links%20(A%20day).html" } //opens the page again
 setTimeout(() => {reloadPage(); }, 1800000); //sets timeout for some time
-function autoOpen(Hour, Minute, Number, link6){ //Sets the function with the variables that change
+function autoOpen(Hour, Minute, Number, link){ //Sets the function with the variables that change
     var now = new Date();
     var Number = new Date(now.getFullYear(), now.getMonth(), now.getDate(), Hour, Minute, 0, 0) - now; // The requested time - the current time = the time till it needs to open
     if (Number <= 0){  //if its below 0
         Number = 86400000 // sets it to a day so that it wont open and will take enough time to reloa and keep delaying it
     } //closing
-    setTimeout(() => {window.location.href = link6; }, Number); } }
+    setTimeout(() => {window.location.href = link; }, Number); } }
