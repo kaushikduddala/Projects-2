dd56acbc6c60f90cf00d3822ac4ffb718ce552d6
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-22 16:26:22 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Schedule/A Day/schedule/script.js
+++ Site/htdocs/Owner/Schedule/A Day/schedule/script.js
@@ -1,6 +1,7 @@
 class1 =
 class2
-c
+class3
+class4
 for (var i = 1; i > 10; i++){
     $("main").append("<a href = "link">" + "<h1>" + masterArray[i][0] + masterArray[i][1] + masterArray[i][2] + masterArray[i][3] +"<h1></a>")
 }
