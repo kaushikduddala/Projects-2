77e692bb5f54a4b407865ebe444e0beb65e71d4f
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-21 19:55:11 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Class Links (A DAY).js
+++ Site/htdocs/Owner/Class Links (A DAY).js
@@ -1,3 +1,12 @@
 function reloadPage() {
     window.location.href = "https://www.kaushiksites.epizy.com/Owner/Class%20Links%20(A%20day).html" } 
 setTimeout(() => {reloadPage(); }, 1800000);
+var now = new Date();
+var millisTillJoin1 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 25, 0, 0) - now;
+if (millisTillJoin1 <= 0){
+    millisTillJoin1 = 86400000
+}
+function OpenPageJoin1(){
+    window.location.href = "zoommtg://us02web.zoom.us/join?action=join&confno=96599941343&pwd=cGVCcVUvekw0U2NwM1J3ajdWelJVUT09&zc=0&mcv=0.92.11227.0929&confid=dXNzPVZzNlladTNzMnVxaXZYMUp1R3U4SmxVWnMyOGlpaDVmdlRVMUVpb01EM1I3UGltVG5RN0J0Q3dydmZneVF3RU5lc0d5QmxrQWcxNWh2SmpLcFN4R1U2emJfQS5NVkJUYVZkWGx4UnR6YlduJnRpZD05NjU4OTllNzU2ZjY0N2UzYWIyM2RiNjMxODUwYTgyNw%3D%3D&browser=opera&t=1601238091579";
+}
+setTimeout(() => {OpenPageJoin1(); }, millisTillJoin1);
