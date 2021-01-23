fd3492d55bc78ef2b78460821c203c412c6cec42
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-23 15:42:53 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Schedule/A Day/script.js
+++ Site/htdocs/Owner/Schedule/A Day/script.js
@@ -4,9 +4,9 @@ class2 = ['Mr. Daniel. ', '9:12', '9:55. ', ' Academy Connect.' ] //class 2
 class3 = ['Ms. Watts. ', '9:59 ', '11:29.', ' GT ILA.'] //class 3
 class4 = ['Mr. Holzer. ', '12:03 ', '12:49.', 'Band(1). '] //class 4
 class5 = ['Mr. Mckellar', '12:03', '12:49', 'Band(2). '] //class 5
-class6 = ['Ms. Boeke', '12:50', '1:33', '7th PAP math. '] //class 
-class7 = ['Coach Green', '2:13', '2:59', 'PE.']
-class8 = ['Ms. Kitchens', '3:00', '3:45', 'Science.']
+class6 = ['Ms. Boeke', '12:50', '1:33', '7th PAP math. '] //class 6
+class7 = ['Coach Green', '2:13', '2:59', 'PE.'] //class 7
+class8 = ['Ms. Kitchens', '3:00', '3:45', 'Science.'] //class 8
 masterArray = [class1, class2, class3, class4, class5, class6, class7, class8]
 for (var i = 1; i < 9; i++){
     $("main").append("<a href = " + links[i] +">" + "<h3>" + masterArray[i][0] + masterArray[i][1] + masterArray[i][2] + masterArray[i][3] +"<h3>" + "</a>" + "<div style='line-height:5%;'>" + "<br>"+  "</div>")
