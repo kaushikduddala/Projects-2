2420754c48a2c76c7a49f5eac30c29727838cbdd
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-23 15:42:06 -0600

    Adding/Removing New Files

--- Site/htdocs/Owner/Schedule/A Day/script.js
+++ Site/htdocs/Owner/Schedule/A Day/script.js
@@ -1,4 +1,4 @@
-/* this is line is really long so i'll just explain it here. So this is all the links in order and its takes later on by  */ links = []
+/* this is line is really long so i'll just explain it here. So this is all the links in order and its takes later on by the   */ links = []
 class1 = ['Mr. Travis.', '8:25 ', '9:08. ', ' Social Studies.']
 class2 = ['Mr. Daniel. ', '9:12', '9:55. ', ' Academy Connect.' ]
 class3 = ['Ms. Watts. ', '9:59 ', '11:29.', ' GT ILA.']
