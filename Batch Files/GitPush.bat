78e92f56b312bcaa6a0ced4d49e80df7cc80197d
Kaushik Duddala <72582623+kaushikduddala@users.noreply.github.com>
2021-01-25 13:04:45 -0600

    Adding/Removing New Files

--- Batch Files/GitPush.bat
+++ Batch Files/GitPush.bat
@@ -1,6 +1,6 @@
 @echo off
-cd C:/users/kaush/documents/github/Projects-2 //sets local directory as the github folder
-git add -A // User and pass are saved
-git commit -m "Adding/Removing New Files" //commit reason
-git push -u //pushing commits
-exit //exits script
+cd C:/users/kaush/documents/github/Projects-2
+git add -A
+git commit -m "Adding/Removing New Files"
+git push -u 
+exit
